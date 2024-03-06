ce2fdb77ce93a6eb4add08e8531e7028031c8eb7
Hidenori Ishikawa <hideishi.magisystem.net@gmail.com>
2024-03-06 13:38:46 +0900

    Mod: refactor makephar.

--- makephar.php
+++ makephar.php
@@ -1,33 +1,45 @@
 <?php
+/**
+ * Copyright (c) 2024 Hidenori ISHIKAWA. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+function createStub(string $target): string
+{
+	return <<<"EOF"
+#!/usr/bin/env php
+<?php
+Phar::mapPhar('{$target}.phar');
+include 'phar://{$target}.phar/index.php';
+__HALT_COMPILER();
+EOF;
+}
+
+function buildPhar(string $target): void
+{
+	error_log("target is {$target}");
+	$pharfile = "{$target}.phar";
+	if (is_file($pharfile)) {
+		unlink($pharfile);
+	}
+	$phar = new Phar($pharfile);
+	$phar->buildFromDirectory(__DIR__ . '/src/', '#/include/[^/]+\.php$#');
+	$phar->addFile(__DIR__ . "/src/{$target}.php", '/index.php');
+	$phar->compressFiles(Phar::GZ);
+	$phar->setStub(createStub($target));
+	$phar->stopBuffering();
+	rename($pharfile, $target);
+	chmod($target, 0755);
+}
 
 if (! isset($argv[1])) {
 	error_log('argv[1] is not set');
 	exit(1);
 }
-
-$script = $argv[1];
-error_log("target is {$script}");
-$pharfile = "{$script}.phar";
-if (is_file($pharfile)) {
-	unlink($pharfile);
-}
-$phar = new Phar($pharfile);
-$phar->buildFromDirectory(__DIR__ . '/src/', '#/include/[^/]+\.php$#');
-$phar->addFile(__DIR__ . "/src/{$script}.php", '/index.php');
-$phar->compressFiles(Phar::GZ);
-$phar->setStub(createStub($script));
-$phar->stopBuffering();
-rename($pharfile, $script);
-chmod($script, 0755);
-
-function createStub($script)
-{
-	$stub = <<<"EOF"
-#!/usr/bin/env php
-<?php
-Phar::mapPhar('{$script}.phar');
-include 'phar://{$script}.phar/index.php';
-__HALT_COMPILER();
-EOF;
-	return $stub;
-}
+buildPhar($argv[1]);
