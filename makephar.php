1a068bbbe5d303a953e59d5d86ad97282b73abc6
Hidenori Ishikawa <hideishi.magisystem.net@gmail.com>
2024-03-06 11:08:28 +0900

    Mod: restructure directory.

--- makephar.php
+++ makephar.php
@@ -7,13 +7,19 @@ if (! isset($argv[1])) {
 
 $script = $argv[1];
 error_log("target is {$script}");
+$pharfile = "{$script}.phar";
+$pattern = '#/(include|' . preg_quote($script, '#') . ')/[^/]+\.php$#';
+var_dump($pattern);
 
+if (is_file($pharfile)) {
+	unlink($pharfile);
+}
 $phar = new Phar("{$script}.phar");
-$phar->buildFromDirectory(__DIR__ . "/src/{$script}/", '#\.php$#');
+$phar->buildFromDirectory(__DIR__ . '/src/', $pattern);
 $phar->compressFiles(Phar::GZ);
 $phar->setStub(createStub($script));
 $phar->stopBuffering();
-rename("{$script}.phar", $script);
+rename($pharfile, $script);
 chmod($script, 0755);
 
 function createStub($script)
@@ -22,7 +28,7 @@ function createStub($script)
 #!/usr/bin/env php
 <?php
 Phar::mapPhar('{$script}.phar');
-include 'phar://{$script}.phar/index.php';
+include 'phar://{$script}.phar/{$script}/index.php';
 __HALT_COMPILER();
 EOF;
 	return $stub;
