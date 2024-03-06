41686c0f77cae92f907bb6af9a41bc045dd0247d
Hidenori Ishikawa <hideishi.magisystem.net@gmail.com>
2024-03-06 11:09:13 +0900

    Mod: refactor ssl_cert_discovery.

---
+++ src/include/SSLCertificateDiscovery.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Copyright (c) 2024 Hidenori ISHIKAWA. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_once __DIR__ . '/NginxConfDirRecursiveFilterIterator.php';
+require_once __DIR__ . '/SSLCertificateCommon.php';
+require_once __DIR__ . '/SSLCertificateVersion.php';
+
+class SSLCertificateDiscovery extends SSLCertificateCommon
+{
+	final const VERSION = SSL_CERTIFICATE_VERSION;
+
+	final const OS_FREEBSD = 'FreeBSD';
+	final const OS_LINUX = 'Linux';
+
+	final const CONF_DIR_FREEBSD = '/usr/local/etc/nginx/conf.d';
+	final const CONF_DIR_LINUX = '/etc/nginx/conf.d';
+	final const CONF_DIR_KUSANAGI = '/etc/opt/kusanagi/nginx/conf.d';
+
+	private array $certs = [];
+	
+	private function getDefaultConfDir(): string
+	{
+		if (self::OS_FREEBSD === PHP_OS) {
+			return self::CONF_DIR_FREEBSD;
+		} elseif (self::OS_LINUX === PHP_OS) {
+			if (false === strpos(PHP_PREFIX, '/kusanagi/')) {
+				return self::CONF_DIR_LINUX;
+			} else {
+				return self::CONF_DIR_KUSANAGI;
+			}
+		} else {
+			return '';
+		}
+	}
+
+	private function parseNginxConf(string $conf): array
+	{
+		$certs = [];
+		$conf = preg_replace('/\s*#.*$/m', '', $conf);
+		$blocks = preg_split('/^\s*server\s*{\s*/m', $conf);
+		foreach ($blocks as $block) {
+			if (preg_match('/^[^#]*server_name\s+([^#;]+)\s*;\s*(?:#.*)?$/m', $block, $match)) {
+				$server_names = preg_split('/\s+/', $match[1]);
+			}
+			if (preg_match('/^[^#]*ssl_certificate\s+([^ #;]+)\s*;\s*(?:#.*)?$/m', $block, $match)) {
+				$cert_path = $match[1];
+			}
+			if (! empty($server_names[0])
+				&& ! empty($cert_path)) {
+				$certs[] = [
+					'{#CERTNAME}' => $server_names[0], 
+					'{#CERTPATH}' => $cert_path,
+				 ];
+			}
+		}
+		return $certs;
+	}
+
+	private function discoverConfDir(string $conf_dir): bool
+	{
+		if (empty($conf_dir)) {
+			$this->messages[] = 'Configuration directory is not specified';
+			return false;
+		} elseif (! is_dir($conf_dir)) {
+			$this->messages[] = 'Configuration directory does not exist';
+			return false;
+		} elseif (! is_readable($conf_dir)) {
+			$this->messages[] = 'Configuration directory is not readable';
+			return false;
+		}
+
+		$cert_paths = [];
+		$conf_dir_iterator = new \RecursiveDirectoryIterator($conf_dir);
+		$filter_iterator = new NginxConfDirRecursiveFilterIterator($conf_dir_iterator);
+		$iterator = new \RecursiveIteratorIterator($filter_iterator);
+		foreach ($iterator as $file_info) {
+			$file_obj = $file_info->openFile('r');
+			foreach ($file_obj as $line) {
+				if (preg_match('/^[^#]*ssl_certificate\s+([^ #;]+)\s*;\s*(?:#.*)?$/', $line, $match)) {
+					$cert_path = $match[1];
+					if (! in_array($cert_path, $cert_paths, true)
+						&& is_file($cert_path)) {
+						$cert_paths[] = $cert_path;
+					}
+				}
+			}
+		}
+		foreach ($cert_paths as $cert_path) {
+			if (is_readable($cert_path)
+				&& false !== ($pem = file_get_contents($cert_path))
+				&& false !== ($x509 = openssl_x509_parse($pem))
+				&& isset($x509['subject'])
+				&& isset($x509['subject']['CN'])) {
+				$this->certs[] = [
+					'{#CERTNAME}' => $x509['subject']['CN'],
+					'{#CERTPATH}' => $cert_path,
+				];
+			}
+		}
+
+		return true;
+	}
+
+	public function __construct(string $conf_dir)
+	{
+		try {
+			$conf_dir = $conf_dir ?: $this->getDefaultConfDir();
+			$this->discoverConfDir($conf_dir);
+		} catch (\Exception $e) {
+			error_log($e);
+		}
+		echo json_encode($this);
+	}
+
+	public function jsonSerialize(): mixed
+	{
+		return $this->certs;
+	}
+}
